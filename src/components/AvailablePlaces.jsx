db9cf5cfe6f405fa161ac9885fc362dc8c95291c
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-12-15 10:53:20 -0300

    feat: 236. handling HTTP Errors

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,41 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import ErrorPage from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsLoading(true);
-      const response = await fetch("http://localhost:3000/places");
-      const data = await response.json();
-      setAvailablePlaces(data.places);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(data.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
+
       setIsLoading(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <ErrorPage title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
