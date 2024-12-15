83a3ef81441878689170c22612fbf0bd29c2a600
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-12-15 11:31:07 -0300

    feat: 239. sending data with POST Request

--- src/http.js
+++ src/http.js
@@ -1,5 +1,10 @@
+const BASE_URL = "http://localhost:3000";
+const BASE_HEADERS = {
+  "Content-Type": "application/json",
+};
+
 export async function fetchAvailablePlaces() {
-  const response = await fetch("http://localhost:3000/places");
+  const response = await fetch(`${BASE_URL}/places`);
   const data = await response.json();
 
   if (!response.ok) {
@@ -7,4 +12,19 @@ export async function fetchAvailablePlaces() {
   }
 
   return data.places;
+}
+
+export async function updateUserPlaces(places) {
+  const response = await fetch(`${BASE_URL}/user-places`, {
+    method: "PUT",
+    body: JSON.stringify({ places }),
+    headers: BASE_HEADERS,
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data");
+  }
+
+  return data.message;
 }
